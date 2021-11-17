5e0d804a68d36d34c1a8c3254003fdbb5b94fcea
zech.emanuel <emanuel.zech@gmail.com>
2021-11-17 10:09:00 +0100

    update

--- kalender/script.js
+++ kalender/script.js
@@ -1,12 +1,48 @@
-calendar();
+var month = [
+  { name: "Jänner", length: 31 },
+  { name: "Februar", length: 28 },
+  { name: "März", length: 31 },
+  { name: "April", length: 30 },
+  { name: "Mai", length: 31 },
+  { name: "Juni", length: 30 },
+  { name: "Juli", length: 31 },
+  { name: "August", length: 31 },
+  { name: "September", length: 30 },
+  { name: "Oktober", length: 31 },
+  { name: "November", length: 30 },
+  { name: "Dezember", length: 31 },
+];
 
-var months = ["Jänner", "Februar", "März", "April", "Mai", "Juni","Juli", "August", "September", "Oktober", "November", "Dezember"]
+let currentMonth = 0;
+let offset = 4;
+let currentDay = 1;
+let newUnit = 0;
+function printMonth() {
+  document.write(month[currentMonth].name + "<br>");
+  document.write("|MO| DI |MI|DO | FR|SA| SO |" + "<br>");
+  for (let index = 0; index < 35; index++) {
+    if (newUnit == 7) {
+      document.write("|");
+      document.write("<br>");
+      newUnit = 0;
+    }
+    if (offset == 0) {
+      if (currentDay < 10) {
+        document.write("| &nbsp;" + currentDay + "    &nbsp;");
+      }else{
+        document.write("|  " + currentDay + "  ");
+      }
 
-
-function calendar(){
-for (let index = 0; index < months.length; index++) {
-    const element = months[index];
-    document.write("MO | DI | MI | DO | FR | SA | SO" + "<br>")
-
+      newUnit++;
+      if (currentDay < month[currentMonth].length) {
+        currentDay++;
+      }else{document.write("|")}
+    
+    } else if (offset > 0) {
+      document.write(" |&nbsp; &nbsp; &nbsp;  ");
+      newUnit++;
+      offset = offset - 1;
+    }
+  }
 }
-}
+printMonth();
