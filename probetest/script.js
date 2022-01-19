3e7b07c1e5138f924c10be05863bb18b7a45403b
zech.emanuel <emanuel.zech@gmail.com>
2022-01-19 10:35:47 +0100

    init fußballseite

---
+++ probetest/script.js
@@ -0,0 +1,15 @@
+function giveOutput() {
+  var größe = document.getElementById("input1").value;
+  var gewicht = document.getElementById("input2").value;
+  var result = (größe * 1.43) / gewicht - 3.4;
+  console.log(result);
+  if (result > 0) {
+    let x = "positive";
+    document.getElementById("output").innerHTML = x;
+  } else if (result < 0) {
+    let x = "negative";
+    document.getElementById("output").innerHTML = x;
+  } else if (result == 0) {
+    document.getElementById("output").innerHTML = 0;
+  }
+}
