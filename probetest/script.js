f0c5174c1cc1e97cb1272c8d2a9d0a5cca0cc909
zech.emanuel <emanuel.zech@gmail.com>
2022-01-20 11:01:41 +0100

    update

--- probetest/script.js
+++ probetest/script.js
@@ -4,12 +4,10 @@ function giveOutput() {
   var result = (größe * 1.43) / gewicht - 3.4;
   console.log(result);
   if (result > 0) {
-    let x = "positive";
-    document.getElementById("output").innerHTML = x;
+    document.getElementById("output").innerHTML = "Positiv";
   } else if (result < 0) {
-    let x = "negative";
-    document.getElementById("output").innerHTML = x;
+    document.getElementById("output").innerHTML = "Negativ";
   } else if (result == 0) {
-    document.getElementById("output").innerHTML = 0;
+    document.getElementById("output").innerHTML = "Null";
   }
 }
